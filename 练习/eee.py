a5e36fb5dffda411f7e1da661dd485fa39bb8d89
LTA <130956559+lta123456@users.noreply.github.com>
2023-04-17 13:31:29 +0800

    Delete 练习 directory

--- 练习/eee.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-s = requests.session()
-HOST = r'http://121.41.14.39:8082/user/list'#'http://ip:port'
-NAME_PSW = {'username':'ka0518','password':'C607D58E3618832F937D80D500A6046C'}
-print(s.get(HOST).json())
